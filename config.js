87261e0af8ac0aad1eed7e0466ef87782c9e350b
Mark Bereczki <markbereczki96@gmail.com>
2020-05-01 18:52:22 +0200

    2.5 dimensional map

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ var config = {
             // image: './path/to/image/source.png',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
-                center: { lon: 19.42883, lat: 47.16100 },
-                zoom: 6.48,
-                pitch: 0.00,
-                bearing: 0.00
+                center: { lon: 19.43735, lat: 47.20254 },
+                zoom: 6.64,
+                pitch: 47.00,
+                bearing: -12.00
             },
             onChapterEnter: [
                 // {
@@ -39,10 +39,10 @@ var config = {
             // image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: { lon: 10.34157, lat: 51.02902 },
-                zoom: 5.60,
-                pitch: 5.00,
-                bearing: 1.00
+                center: { lon: 9.82083, lat: 51.38690 },
+                zoom: 5.76,
+                pitch: 58.00,
+                bearing: -7.16
             },
             onChapterEnter: [],
             onChapterExit: []
@@ -53,10 +53,10 @@ var config = {
             // image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: { lon: 14.23296, lat: 41.74773 },
-                zoom: 5.60,
-                pitch: 10.00,
-                bearing: 2.00
+                center: { lon: 13.73695, lat: 41.97617 },
+                zoom: 5.76,
+                pitch: 58.00,
+                bearing: -7.16
             },
             onChapterEnter: [],
             onChapterExit: []
